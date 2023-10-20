cc6144164b1cd9ce937633d1b58929270cd9fb88
Julianna Alencar <julianna.dantas@hotmail.com>
2023-10-20 19:06:54 -0300

    Atualizando exercicios de PY

--- curso_em_video_py/ex018.py
+++ curso_em_video_py/ex018.py
@@ -1,9 +1,9 @@
-import math
+from math import radians, sin, cos, tan
 
 angulo = float(input('Informe o ângulo que deseja calcular: '))
+seno_angulo = sin(radians(angulo))
+cosseno_angulo = cos(radians(angulo))
+tangente_angulo = tan(radians(angulo))
 
-seno_angulo = math.sin(angulo)
-cosseno_angulo = math.cos(angulo)
-
-print(f'O angulo informado foi {angulo}, seu seno é {seno_angulo:.2f} e o cosseno é {cosseno_angulo:.2f}')
+print(f'O angulo informado foi {angulo}, seu seno é {seno_angulo:.2f}, o cosseno é {cosseno_angulo:.2f}, e a tangente é {tangente_angulo:.2f}')
 
---
+++ curso_em_video_py/ex021.py
@@ -0,0 +1,8 @@
+import pygame
+pygame.init()
+pygame.mixer.music.load('tiro-perfeito.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
+
+
